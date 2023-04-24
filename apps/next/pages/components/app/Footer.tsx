dff20c26645646823abd57f065ae1a09c4908909
biclighter81 <moritzben.becker@outlook.de>
2023-04-24 16:59:31 +0200

    frontend bootstrap + typeorm setup

---
+++ apps/next/pages/components/app/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="h-[75px] bg-gray-200 flex justify-center items-center">
+        Footer
+      </div>
+    </>
+  );
+}
--- packages/types/index.ts
+++
@@ -1,3 +0,0 @@
-export interface ShareTest {
-  id: string;
-}
