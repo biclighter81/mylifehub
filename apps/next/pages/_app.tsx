69388e1a2f40c3128c9b517e021e1ad7dce6f513
bic_lighter81 <moritzben.becker@outlook.de>
2023-06-19 11:30:28 +0200

    test

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -2,11 +2,22 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/app/Layout';
 import { SessionProvider } from 'next-auth/react';
+import { useEffect } from 'react';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const colors = ['#f00', '#0f0', '#00f', '#ff0', '#f0f', '#0ff'];
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      document.body.style.backgroundColor = randomColor;
+    }, 500);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <SessionProvider session={pageProps.session} refetchInterval={10}>
       <Layout>
